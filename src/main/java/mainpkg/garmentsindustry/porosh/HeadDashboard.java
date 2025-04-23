7bf5778b81bb6ae8f708cec47172d7a3f5a103da
Porosh555 <sporserporosh32@gmail.com>
2025-04-23 23:29:26 +0600

    first comit2

--- src/main/java/mainpkg/garmentsindustry/porosh/HeadDashboard.java
+++ src/main/java/mainpkg/garmentsindustry/porosh/HeadDashboard.java
@@ -2,6 +2,8 @@ package mainpkg.garmentsindustry.porosh;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -18,40 +20,121 @@ public class HeadDashboard
 
     @javafx.fxml.FXML
     public void u1g2(ActionEvent actionEvent) {
-        try{
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AllUsersFxml.fxml"));
-                Scene scene = new Scene(fxmlLoader.load());
-                stage.setTitle("Garments Industry");
-                stage.setScene(scene);
-                stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g2.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
         }
     }
 
     @javafx.fxml.FXML
     public void u1g1(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g2.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u1g8(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g8.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u1g7(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g7.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u1g4(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g4.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+    }
     }
 
     @javafx.fxml.FXML
     public void u1g3(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g3.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u1g6(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g6.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u1g5(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u1g5.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u1g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/mainpkg/garmentsindustry/porosh/ManufacturerDashboard.java
+++ src/main/java/mainpkg/garmentsindustry/porosh/ManufacturerDashboard.java
@@ -1,6 +1,13 @@
 package mainpkg.garmentsindustry.porosh;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class ManufacturerDashboard
 {
@@ -10,33 +17,120 @@ public class ManufacturerDashboard
 
     @javafx.fxml.FXML
     public void u2g1(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g1.fxml"));
+            Parent root = fxmlLoader.load();
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("u2g2");
+            stage.show();
+        }catch (IOException exception){
+            exception.printStackTrace();
     }
 
     @javafx.fxml.FXML
     public void u2g7(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g7.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u2g3");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g6(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g6.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g8(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g8.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g3(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g3.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g2(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g2.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g5(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g5.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void u2g4(ActionEvent actionEvent) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainpkg/garmentsindustry/porosh/u2g4.fxml"));
+                Parent root = fxmlLoader.load();
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("u1g2");
+                stage.show();
+            }catch (IOException exception){
+                exception.printStackTrace();
+        }
     }
 }
