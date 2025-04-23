2ae6ea5184a0d40372752570d33efc4ee2888642
tasrian85 <salmatasrian85@gmail.com>
2025-04-23 19:48:35 +0600

    ,

---
+++ src/main/java/mainpkg/garmentsindustry/GeneralMember.java
@@ -0,0 +1,8 @@
+package mainpkg.garmentsindustry;
+
+public class GeneralMember {
+    public static String companyPolicyNote = "............";
+    private String workerId;
+    private String name;
+    private double salary;
+}
---
+++ src/main/java/mainpkg/garmentsindustry/Worker.java
@@ -0,0 +1,9 @@
+package mainpkg.garmentsindustry;
+
+public class Worker {
+    public static String companyPolicyNote = "";
+    private String workerId;
+    private String name;
+    private double salary;
+
+}
